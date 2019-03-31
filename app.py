10b1decf2c8eeb2d20d5c2c71894b70fb8f52277
sa--sondarkar <saisondarkar18@gmail.com>
2019-03-31 07:49:17 +0530

    Added

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+#need to 'conda install flask' for this to work
+from flask import Flask, abort, jsonify, request
+import pickle
+
+
+my_random_forest = pickle.load(open("hr_data_ml.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def make_predict():
+    #all kinds of error checking should go here
+    data = request.get_json(force=True)
+    #convert our json to a numpy array
+    predict_request = [data['nu'],data['ps1'],data['ps2'],data['ps3'],data['pm1'],data['pm2'],data['pm3'],data['sal'],data['p']] 
+    predict_request = np.array(predict_request).reshape(1,-1)
+    #np array goes into random forest, prediction comes out
+    y_hat = my_random_forest.predict(predict_request)
+    #return our prediction
+    output = {'y_hat': y_hat[0]}
+    print(output)
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
