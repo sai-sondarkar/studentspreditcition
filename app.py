4b7ed4e247aac16482b8d8fd234625d751e3cee7
sa--sondarkar <saisondarkar18@gmail.com>
2019-04-27 22:57:04 +0530

    Added the HR thing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, abort, jsonify, request
 import pickle
 
 
-my_random_forest = pickle.load(open("hr_data_ml.pkl", "rb"))
+my_random_forest = pickle.load(open("students_data_ml.pkl", "rb"))
 
 app = Flask(__name__)
 
@@ -12,13 +12,26 @@ app = Flask(__name__)
 def make_predict():
     #all kinds of error checking should go here
     data = request.get_json(force=True)
+    ssc = data["ssc"]
+    hsc = data["hsc"]
+    set =0
+
+    if  ssc> 70 and hsc>70:
+        set = 1
+    elif ssc> 60 and hsc>60:
+        set = 2
+    else:
+        set = 3
+
+
     #convert our json to a numpy array
-    predict_request = [data['nu'],data['ps1'],data['ps2'],data['ps3'],data['pm1'],data['pm2'],data['pm3'],data['sal'],data['p']] 
-    predict_request = np.array(predict_request).reshape(1,-1)
+    # predict_request = [data['nu'],data['ps1'],data['ps2'],data['ps3'],data['pm1'],data['pm2'],data['pm3'],data['sal'],data['p']] 
+    # predict_request = np.array(predict_request).reshape(1,-1)
     #np array goes into random forest, prediction comes out
-    y_hat = my_random_forest.predict(predict_request)
+    # y_hat = my_random_forest.predict(predict_request)
     #return our prediction
-    output = {'min_months_time_span': y_hat[0]*60}
+    y_hat = set
+    output = {'job_set': y_hat}
     print(output)
     return jsonify(results=output)
 
