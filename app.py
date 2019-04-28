44dd10f1b13a0ad44a20d5e5eae99a0eb73f91ee
sa--sondarkar <saisondarkar18@gmail.com>
2019-04-28 09:47:01 +0530

    Last Added

--- app.py
+++ app.py
@@ -14,23 +14,14 @@ def make_predict():
     data = request.get_json(force=True)
     ssc = data["ssc"]
     hsc = data["hsc"]
-    set =0
-
-    if  ssc> 70 and hsc>70:
-        set = 1
-    elif ssc> 60 and hsc>60:
-        set = 2
-    else:
-        set = 3
-
 
     #convert our json to a numpy array
-    # predict_request = [data['nu'],data['ps1'],data['ps2'],data['ps3'],data['pm1'],data['pm2'],data['pm3'],data['sal'],data['p']] 
-    # predict_request = np.array(predict_request).reshape(1,-1)
-    #np array goes into random forest, prediction comes out
-    # y_hat = my_random_forest.predict(predict_request)
+    predict_request = [ssc,hsc]
+    predict_request = np.array(predict_request).reshape(1,-1)
+    # np array goes into random forest, prediction comes out
+    y_hat = my_random_forest.predict(predict_request)
     #return our prediction
-    y_hat = set
+
     output = {'job_set': y_hat}
     print(output)
     return jsonify(output)
